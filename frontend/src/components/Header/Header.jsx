cdccaf6e402beb40df103604068f961c041fbb74
rajrawat37 <rawatraj995@gmail.com>
2024-08-06 15:45:18 +0530

    feat:header-component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, perferendis.
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eos eveniet commodi
+               ex alias repellendus placeat facere, vero fuga quo voluptate optio iste, atque possimus 
+               veniam explicabo quia. Culpa, facere.</p>
+
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
