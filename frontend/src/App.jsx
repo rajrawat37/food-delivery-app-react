9cabefe79e1cd432753c1ef0f3fc50ea93d37811
rajrawat37 <rawatraj995@gmail.com>
2024-08-12 11:49:45 +0530

    feat: signup component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar';
 import { Route,Routes } from 'react-router-dom';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
+import LoginPopup from './components/LoginPopup/LoginPopup';
 
 const App = () => {
+
+  
+  const [showLogin,setShowLogin] = useState(false);
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
+    
      <div class='app'>
-        <Navbar/>
+        <Navbar setShowLogin={setShowLogin}/>
 
         <Routes>
           <Route path='/' element={<Home/>}/>
