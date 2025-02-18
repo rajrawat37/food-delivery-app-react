703d6dffc48a2daa1648003bbe3629500d9121de
rajrawat37 <rawatraj995@gmail.com>
2025-02-18 20:47:47 +0530

    feat:Add cart to DB

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,10 +1,31 @@
 import userModel from "../models/userModel.js";
 
 // Add items to user Cart
-const addToCart = async (req, res) => {};
+const addToCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById({ _id: req.body.userId });
+    let cartData = await userData.cartData;
+
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId] += 1;
+    }
+
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+
+    res.json({ success: true, message: "Added to Cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 //Remove items from user Cart
-const removeFromCart = async (req, res) => {};
+const removeFromCart = async (req, res) => {
+  try {
+  } catch (error) {}
+};
 
 //Fetch user Cart data
 const getCart = async (req, res) => {};
