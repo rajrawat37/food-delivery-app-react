7a5c3da73b8e025559686630eede679de01c7e2a
rajrawat37 <rawatraj995@gmail.com>
2025-02-18 19:20:27 +0530

    fix: controller changes

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -4,60 +4,53 @@ import fs from "fs";
 //add food item
 
 const addFood = async (req, res) => {
-
   let image_filename = `${req.file.filename}`;
 
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
 
-      const food = new foodModel({
-        name: req.body.name,
-        description: req.body.description,
-        price: req.body.price,
-        category: req.body.category,
-        image: image_filename,
-      });
-
-      try {
-          await food.save();
-          res.json({ success: true, message: "Food Added" , data:food});
-      } catch (error) {
-          console.log(error);
-          res.json({ success: false, message: "Error" });
-      }
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food Added", data: food });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
 };
 
 //all food list
 
-const listFood = async(req,res) => {
-
-   try {   
-        const foods = await foodModel.find({});
-        res.json({success:true,data:foods});
-   } catch (error) {
-        console.log(error);
-        res.json({success:true,message:"Error"});
-   }
-}
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: true, message: "Error" });
+  }
+};
 
 //remove food items
 
-const removeFood = async(req,res) =>{
-
+const removeFood = async (req, res) => {
   try {
-     const food=await foodModel.findById(req.body.id);
+    const food = await foodModel.findById(req.body.id);
 
-     //to delete it from folder
-     fs.unlink(`uploads/${food.image}`,()=>{});
-     
-     //now delete it from mongodb database
-      await foodModel.findByIdAndDelete(req.body.id);
-      res.json({success:true,message:"Food Removed"});
+    //to delete it from folder
+    fs.unlink(`uploads/${food.image}`, () => {});
 
+    //now delete it from mongodb database
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Food Removed" });
   } catch (error) {
-      console.log(error);
-      res.json({success:false,message:"error"});
+    console.log(error);
+    res.json({ success: false, message: "error" });
   }
-}
+};
 
-
-
-export { addFood,listFood,removeFood };
+export { addFood, listFood, removeFood };
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,49 +1,45 @@
-import express from 'express';
-import {addFood, listFood, removeFood} from '../controllers/foodController.js'
+import express from "express";
+import {
+  addFood,
+  listFood,
+  removeFood,
+} from "../controllers/foodController.js";
 import multer from "multer";
 
-
-
-//foodRouter is a router object that is created using express.Router(). 
+//foodRouter is a router object that is created using express.Router().
 //This router would contain specific routes related to food, such as listing food items, adding new food items, updating food items, etc.
 //The router allows you to modularize and organize your routes related to a specific domain (in this case, food).
 
 //so basically to modularize the routes for food a foodRouter file is made here.
 
 const foodRouter = express.Router();
-//Once a router is defined, we need to "mount" it to a specific path in our main application using app.use(). 
+//Once a router is defined, we need to "mount" it to a specific path in our main application using app.use().
 //This allows all routes defined in the router to be accessible under a common path prefix.
-
-
 
 // Image Storage Engine
 // Multer is a middleware for handling multipart/form-data
 
-// multipart/form-data is one of the encoding types specified in HTML forms. 
+// multipart/form-data is one of the encoding types specified in HTML forms.
 // It is used when we need to upload files along with other form data (like text fields) to a server.
 
-//Without Multer, handling multipart/form-data would be quite cumbersome 
+//Without Multer, handling multipart/form-data would be quite cumbersome
 //because we would need to manually parse the incoming request, extract the files, handle storage.
 
 const storage = multer.diskStorage({
-    destination: "uploads",
-    filename:(req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`);
-    }
-})
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
 
+const upload = multer({ storage: storage });
 
-
-const upload = multer({storage:storage});
-
-foodRouter.post("/add",upload.single("image"),addFood);
-foodRouter.get("/list",listFood);
-foodRouter.post("/remove",removeFood);
-
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
 
-
-//express.Router() is a powerful feature in Express.js that allows us to create modular route handlers. 
-//By using routers, we can structure our application in a way that is organized, maintainable, and scalable. 
+//express.Router() is a powerful feature in Express.js that allows us to create modular route handlers.
+//By using routers, we can structure our application in a way that is organized, maintainable, and scalable.
 //Each router can handle a specific set of routes, and these can be mounted onto the main application with specific path prefixes.
