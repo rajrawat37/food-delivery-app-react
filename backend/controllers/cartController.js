89e5cf4edcb1ac91878766995d13563c8f25904c
rajrawat37 <rawatraj995@gmail.com>
2025-02-18 20:51:24 +0530

    feat:Remove cart from DB

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -24,7 +24,19 @@ const addToCart = async (req, res) => {
 //Remove items from user Cart
 const removeFromCart = async (req, res) => {
   try {
-  } catch (error) {}
+    let userData = await userModel.findById({ _id: req.body.userId });
+    let cartData = await userData.cartData;
+
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Removed from Cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
 };
 
 //Fetch user Cart data
